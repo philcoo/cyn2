14a15dbc6ccd00577e9610bd1ac04740b051d498
Philco Michel <phil9231@gmail.com>
2016-10-31 02:20:17 -0400

    side bar menu adde in the home page

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -5,3 +5,12 @@
  $(document).ready(function(){
       $('.slider').slider({full_width: true});
     });
+
+ $('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 240
+      edge: 'right', // Choose the horizontal origin
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true // Choose whether you can drag to open on touch screens
+    }
+  );
+        
