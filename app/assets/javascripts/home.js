dd6feff9bc08047c67aef9703d2867412a27e999
Philco Michel <phil9231@gmail.com>
2017-03-01 06:02:14 -0500

    hungermen update

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -3,7 +3,23 @@
  $(document).ready(function(){
       $('.slider').slider({full_width: true});
     });
-  $('.button-collapse').sideNav('show');
+  
+$(document).ready(function(){
+	$('.hamburger-shell').click(function(){
+		$('#menu').slideToggle(300);
+		$('.top').toggleClass('rotate');
+		$('.middle').toggleClass('rotate-back');
+		$('.menu-name').toggleClass('bump');
+		$('.bg-cover').toggleClass('reveal');
+	});
+	$('.bg-cover').click(function(){
+		$('#menu').slideToggle(300);
+		$('.top').toggleClass('rotate');
+		$('.middle').toggleClass('rotate-back');
+		$('.menu-name').toggleClass('bump');
+		$('.bg-cover').toggleClass('reveal');
+	})
+});
 
 
 
